59de1622530d3ed0d217a9d65d31457f5bc6a87e
farzaadshojaei <farzaadshojaei@gmail.com>
2023-10-25 22:57:58 +0330

    Adding Duration

--- BeginningProject/script1.js
+++ BeginningProject/script1.js
@@ -7,7 +7,18 @@
 // Call means get
 
 import http from 'k6/http';
+//RampUp
 
+export let options={
+stages:[
+{duration: '10s', target: 5},
+{duration: '20s', target: 3},
+
+
+]
+
+
+}
 
 export default function(){
     http.get('https://www.google.com/');
